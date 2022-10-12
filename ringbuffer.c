db8a4ab1e688b1bb3bb57db14310283859b7f181
Wang Jingxin <wjx7708@126.com>
2022-10-12 09:10:19 +0800

    expand to dynamic allocation version,

--- ringbuffer.c
+++ ringbuffer.c
@@ -1,75 +1,193 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
 #include "ringbuffer.h"
 
 /**
  * @file
  * Implementation of ring buffer functions.
+ *
+ * 可动态分配队列大小版本，可兼容共享内存方式使用。
+ * 王景鑫 2022/10/10
  */
 
-void ring_buffer_init(ring_buffer_t *buffer) {
-  buffer->tail_index = 0;
-  buffer->head_index = 0;
+void ring_buffer_init(ring_buffer_t *buffer)
+{
+    buffer->tail_index = 0;
+    buffer->head_index = 0;
 }
 
-void ring_buffer_queue(ring_buffer_t *buffer, char data) {
-  /* Is buffer full? */
-  if(ring_buffer_is_full(buffer)) {
-    /* Is going to overwrite the oldest byte */
-    /* Increase tail index */
-    buffer->tail_index = ((buffer->tail_index + 1) & RING_BUFFER_MASK);
-  }
+ring_buffer_size_t ring_buffer_calc_size(size_t buffer_length)
+{
+    int i;
+    //长度检查
+    if (buffer_length > RING_BUFFER_SIZE)
+    {
+        fprintf(stderr, "%s -- ring_buffer_size exceed max range of RING_BUFFER_SIZE(%d).\n", __func__, RING_BUFFER_SIZE);
+        return 0;
+    }
 
-  /* Place data in buffer */
-  buffer->buffer[buffer->head_index] = data;
-  buffer->head_index = ((buffer->head_index + 1) & RING_BUFFER_MASK);
+    for (i = 1; i <= RING_BUFFER_SIZE; i *= 2)
+    {
+        if (buffer_length <= i)
+        {
+            buffer_length = i;
+            // printf("buffer_length = %lu\n",buffer_length);
+            //返回地址块大小包含结构体占用的部分。
+            return (buffer_length + (size_t)sizeof(ring_buffer_t));
+        }
+    }
 }
 
-void ring_buffer_queue_arr(ring_buffer_t *buffer, const char *data, ring_buffer_size_t size) {
-  /* Add bytes; one by one */
-  ring_buffer_size_t i;
-  for(i = 0; i < size; i++) {
-    ring_buffer_queue(buffer, data[i]);
-  }
+ring_buffer_t *ring_buffer_new(ring_buffer_size_t buffer_length)
+{
+
+    ring_buffer_t *buffer = NULL;
+
+    buffer_length = ring_buffer_calc_size(buffer_length);
+
+    //使用一次malloc分配内存，大小是sizeof(ring_buffer_t) + sizeof(buffer_cap*sizeof(char))
+    //然后把结构体首地址赋给结构体指针buffer，数组首地址赋给buffer->buffer_array。
+    buffer = (ring_buffer_t *)malloc(buffer_length * sizeof(char));
+
+    // printf("sizeof ring_buffer_t:%d, sizeof buffer_array:%d.\n",sizeof(ring_buffer_t),(ring_buffer_size * sizeof(char)));
+    // printf("malloc, buffer addr %p -> %d (ring_buffer_t).\n",&buffer,buffer);
+    // printf("tail addr of malloc:%d.\n",(int)buffer + sizeof(ring_buffer_t) + ring_buffer_size * sizeof(char));
+
+    if (buffer == NULL)
+    {
+        fprintf(stderr, "%s -- malloc failed:%s\n", __func__, strerror(errno));
+        free(buffer);
+        return (ring_buffer_t *)NULL;
+    }
+
+    // printf("addr of buffer++:%d.\n",buffer+1);
+    // buffer+1得到结构体后面的地址，将其类型转换为(char*)后赋值给buffer->array
+    buffer->buffer_array = (char *)(buffer + 1);
+    buffer->buffer_cap = buffer_length - sizeof(ring_buffer_t);
+
+    //设置成员变量的值
+    ring_buffer_init(buffer);
+
+    return buffer;
 }
 
-uint8_t ring_buffer_dequeue(ring_buffer_t *buffer, char *data) {
-  if(ring_buffer_is_empty(buffer)) {
-    /* No items */
-    return 0;
-  }
-  
-  *data = buffer->buffer[buffer->tail_index];
-  buffer->tail_index = ((buffer->tail_index + 1) & RING_BUFFER_MASK);
-  return 1;
+void ring_buffer_destroy(ring_buffer_t **buffer)
+{
+
+    //检查buffer指针和buffer->buf_array指针是否有效
+    if (*buffer == NULL)
+    {
+        fprintf(stderr, "%s -- ring_buffer_t ptr is NULL.\n", __func__);
+        return;
+    }
+
+    free(*buffer);
+    *buffer = NULL;
 }
 
-ring_buffer_size_t ring_buffer_dequeue_arr(ring_buffer_t *buffer, char *data, ring_buffer_size_t len) {
-  if(ring_buffer_is_empty(buffer)) {
-    /* No items */
-    return 0;
-  }
+ring_buffer_t *ring_buffer_attach(void *addr, ring_buffer_size_t length)
+{
+    ring_buffer_t *buffer;
+    //判断传入参数是否为空值。
+    if (addr == NULL)
+    {
+        fprintf(stderr, "%s paramater *addr is NULL.\n", __func__);
+        return NULL;
+    }
 
-  char *data_ptr = data;
-  ring_buffer_size_t cnt = 0;
-  while((cnt < len) && ring_buffer_dequeue(buffer, data_ptr)) {
-    cnt++;
-    data_ptr++;
-  }
-  return cnt;
+    buffer = addr;
+    buffer->buffer_array = (char *)(buffer + 1);
+    buffer->buffer_cap = length - sizeof(ring_buffer_t);
+
+    //设置成员变量的值
+    ring_buffer_init(buffer);
+
+    return buffer;
 }
 
-uint8_t ring_buffer_peek(ring_buffer_t *buffer, char *data, ring_buffer_size_t index) {
-  if(index >= ring_buffer_num_items(buffer)) {
-    /* No items at index */
-    return 0;
-  }
-  
-  /* Add index to pointer */
-  ring_buffer_size_t data_index = ((buffer->tail_index + index) & RING_BUFFER_MASK);
-  *data = buffer->buffer[data_index];
-  return 1;
+void ring_buffer_queue(ring_buffer_t *buffer, char data)
+{
+    /* Is buffer full? */
+    if (ring_buffer_is_full(buffer))
+    {
+        /* Is going to overwrite the oldest byte */
+        /* Increase tail index */
+        buffer->tail_index = ((buffer->tail_index + 1) & (buffer->buffer_cap - 1));
+    }
+
+    /* Place data in buffer */
+    buffer->buffer_array[buffer->head_index] = data;
+    buffer->head_index = ((buffer->head_index + 1) & (buffer->buffer_cap - 1));
+}
+
+uint8_t ring_buffer_queue_arr(ring_buffer_t *buffer, const char *data, ring_buffer_size_t size)
+{
+    /* Add bytes; one by one */
+    /* 增加入队列数据大小的判断，超过队列容量则失败返回。 */
+    if (size > buffer->buffer_cap)
+    {
+        fprintf(stderr, "%s -- queue array size exceed buffer size.\n", __func__);
+        return 0;
+    }
+
+    ring_buffer_size_t i;
+    for (i = 0; i < size; i++)
+    {
+        ring_buffer_queue(buffer, data[i]);
+    }
+    return 1;
+}
+
+uint8_t ring_buffer_dequeue(ring_buffer_t *buffer, char *data)
+{
+    if (ring_buffer_is_empty(buffer))
+    {
+        /* No items */
+        return 0;
+    }
+
+    *data = buffer->buffer_array[buffer->tail_index];
+    buffer->tail_index = ((buffer->tail_index + 1) & (buffer->buffer_cap - 1));
+    return 1;
+}
+
+ring_buffer_size_t ring_buffer_dequeue_arr(ring_buffer_t *buffer, char *data, ring_buffer_size_t len)
+{
+    if (ring_buffer_is_empty(buffer))
+    {
+        /* No items */
+        return 0;
+    }
+
+    char *data_ptr = data;
+    ring_buffer_size_t cnt = 0;
+    while ((cnt < len) && ring_buffer_dequeue(buffer, data_ptr))
+    {
+        cnt++;
+        data_ptr++;
+    }
+    return cnt;
+}
+
+uint8_t ring_buffer_peek(ring_buffer_t *buffer, char *data, ring_buffer_size_t index)
+{
+    if (index >= ring_buffer_num_items(buffer))
+    {
+        /* No items at index */
+        return 0;
+    }
+
+    /* Add index to pointer */
+    ring_buffer_size_t data_index = ((buffer->tail_index + index) & (buffer->buffer_cap - 1));
+    *data = buffer->buffer_array[data_index];
+    return 1;
 }
 
 extern inline uint8_t ring_buffer_is_empty(ring_buffer_t *buffer);
 extern inline uint8_t ring_buffer_is_full(ring_buffer_t *buffer);
 extern inline ring_buffer_size_t ring_buffer_num_items(ring_buffer_t *buffer);
-
+extern inline void ring_buffer_detach(ring_buffer_t **buffer);
--- ringbuffer.h
+++ ringbuffer.h
@@ -1,44 +1,34 @@
 #include <inttypes.h>
+#include <stddef.h>
 
 /**
  * @file
- * Prototypes and structures for the ring buffer module.
+ * 对环形队列进行了改写，使它可以支持可变长度的队列。
+ * 由于算法原理限制，队列长度仍然需要保持是2的整数次幂。
+ * 增加了ring_buffer_new函数，用于环形队列对象的分配。
+ * 相对应的，增加了ring_buffer_destroy函数用于队列对象的释放。
+ * 为了适用于共享内存工作环境，增加了三个函数，分别是
+ * ring_buffer_calc_size
+ * ring_buffer_attach
+ * ring_buffer_detach
+ * 目前队列最大可设置长度为32768个char。超过该长度将发生异常。
+ * 队列的实际可利用长度为设置长度-1，需要予以注意。
+ * 王景鑫   2022/10/10
  */
 
 #ifndef RINGBUFFER_H
 #define RINGBUFFER_H
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
-/**
- * The size of a ring buffer.
- * Due to the design only <tt> RING_BUFFER_SIZE-1 </tt> items
- * can be contained in the buffer.
- * The buffer size must be a power of two.
-*/
-#define RING_BUFFER_SIZE 128
-
-#if (RING_BUFFER_SIZE & (RING_BUFFER_SIZE - 1)) != 0
-#error "RING_BUFFER_SIZE must be a power of two"
-#endif
+// 最大允许队列长度
+#define RING_BUFFER_SIZE 32768 // 2^15
 
 /**
  * The type which is used to hold the size
  * and the indicies of the buffer.
  * Must be able to fit \c RING_BUFFER_SIZE .
+ * **将其定义为uint32_t可以兼容更大的长度，需要同时修改RING_BUFFER_SIZE的宏定义为2^31次幂**
  */
-typedef uint8_t ring_buffer_size_t;
-
-/**
- * Used as a modulo operator
- * as <tt> a % b = (a & (b − 1)) </tt>
- * where \c a is a positive index in the buffer and
- * \c b is the (power of two) size of the buffer.
- */
-#define RING_BUFFER_MASK (RING_BUFFER_SIZE-1)
+typedef uint16_t ring_buffer_size_t;
 
 /**
  * Simplifies the use of <tt>struct ring_buffer_t</tt>.
@@ -50,13 +40,22 @@ typedef struct ring_buffer_t ring_buffer_t;
  * The buffer contains a buffer array
  * as well as metadata for the ring buffer.
  */
-struct ring_buffer_t {
-  /** Buffer memory. */
-  char buffer[RING_BUFFER_SIZE];
-  /** Index of tail. */
-  ring_buffer_size_t tail_index;
-  /** Index of head. */
-  ring_buffer_size_t head_index;
+struct ring_buffer_t
+{
+    /**
+     * Buffer memory.
+     * 用指针方式实现，在ring_buffer_new函数或者ring_buffer_attach中进行地址绑定。
+     * */
+    char *buffer_array;
+    /* 队列缓冲的大小级别，2的整数幂次 */
+    /**
+     * 同时，这个变量也作为mask使用。mask = buffer_cap - 1
+     */
+    ring_buffer_size_t buffer_cap;
+    /** Index of tail. */
+    ring_buffer_size_t tail_index;
+    /** Index of head. */
+    ring_buffer_size_t head_index;
 };
 
 /**
@@ -65,6 +64,45 @@ struct ring_buffer_t {
  * @param buffer The ring buffer to initialize.
  */
 void ring_buffer_init(ring_buffer_t *buffer);
+
+/**
+ * @brief 计算需要分配给ring_buffer_t结构体的内存大小，以sizeof(char)为单位。
+ * 返回值是可以容纳length个char的最小2的整数次幂 + ring_buffer_t结构体大小。
+ * @param length - 需要ring_buffer环形队列容纳的容量大小,以sizeof(char)为单位。。
+ * @return 需要分配的内存大小，以sizeof(char)为单位。
+ *
+ */
+ring_buffer_size_t ring_buffer_calc_size(size_t length);
+
+/**
+ * 初始化ring_buffer结构体，并返回该结构体对象的地址。
+ * 如果初始化失败，则返回NULL。
+ * @param buffer_length 申请分配队列的大小，其大小应不超过宏定义RING_BUFFER_SIZE。实际分配的队列大小为大于buffer_length的最小2的整数次幂。
+ * @return 初始化完成的ring_buffer_t结构体对象。
+ * 王景鑫 2022/10/10
+ *
+ */
+ring_buffer_t *ring_buffer_new(ring_buffer_size_t buffer_length);
+
+/**
+ * @brief 销毁不再使用的队列对象。
+ * @param buffer 将要销毁的ring_buffer_t对象指针的地址，请注意这是一个二级指针，需要传递结构体指针的地址。
+ * 传递二级指针的目的是，在释放内存对象的同时将该指针置为NULL，以防止后续程序误用。
+ *
+ * 王景鑫 2002/10/10
+ *
+ */
+void ring_buffer_destroy(ring_buffer_t **buffer);
+
+/**
+ * @brief 使用传入的内存块初始化ring_buffer_t并返回该结构体实例地址。
+ * 此函数需传入已经分配好的内存块。 * 该内存块大小应使用ring_buffer_alloc_calculate函数计算获得。
+ * 此函数的典型使用场景是进程间使用共享内存方式通信，并在分配好的共享内存中放置环形队列。
+ * @param addr - 已经分配好的地址空间首地址，无类型指针。
+ * @param length - 环形队列的长度。
+ * @return 返回初始化好的ring_buffer对象地址。
+ */
+ring_buffer_t *ring_buffer_attach(void *addr, ring_buffer_size_t length);
 
 /**
  * Adds a byte to a ring buffer.
@@ -78,8 +116,9 @@ void ring_buffer_queue(ring_buffer_t *buffer, char data);
  * @param buffer The buffer in which the data should be placed.
  * @param data A pointer to the array of bytes to place in the queue.
  * @param size The size of the array.
+ * @return 1 - success, 0 - fail, exceed buffer size.
  */
-void ring_buffer_queue_arr(ring_buffer_t *buffer, const char *data, ring_buffer_size_t size);
+uint8_t ring_buffer_queue_arr(ring_buffer_t *buffer, const char *data, ring_buffer_size_t size);
 
 /**
  * Returns the oldest byte in a ring buffer.
@@ -106,14 +145,14 @@ ring_buffer_size_t ring_buffer_dequeue_arr(ring_buffer_t *buffer, char *data, ri
  */
 uint8_t ring_buffer_peek(ring_buffer_t *buffer, char *data, ring_buffer_size_t index);
 
-
 /**
  * Returns whether a ring buffer is empty.
  * @param buffer The buffer for which it should be returned whether it is empty.
  * @return 1 if empty; 0 otherwise.
  */
-inline uint8_t ring_buffer_is_empty(ring_buffer_t *buffer) {
-  return (buffer->head_index == buffer->tail_index);
+inline uint8_t ring_buffer_is_empty(ring_buffer_t *buffer)
+{
+    return (buffer->head_index == buffer->tail_index);
 }
 
 /**
@@ -121,8 +160,9 @@ inline uint8_t ring_buffer_is_empty(ring_buffer_t *buffer) {
  * @param buffer The buffer for which it should be returned whether it is full.
  * @return 1 if full; 0 otherwise.
  */
-inline uint8_t ring_buffer_is_full(ring_buffer_t *buffer) {
-  return ((buffer->head_index - buffer->tail_index) & RING_BUFFER_MASK) == RING_BUFFER_MASK;
+inline uint8_t ring_buffer_is_full(ring_buffer_t *buffer)
+{
+    return ((buffer->head_index - buffer->tail_index) & (buffer->buffer_cap - 1)) == (buffer->buffer_cap - 1);
 }
 
 /**
@@ -130,12 +170,21 @@ inline uint8_t ring_buffer_is_full(ring_buffer_t *buffer) {
  * @param buffer The buffer for which the number of items should be returned.
  * @return The number of items in the ring buffer.
  */
-inline ring_buffer_size_t ring_buffer_num_items(ring_buffer_t *buffer) {
-  return ((buffer->head_index - buffer->tail_index) & RING_BUFFER_MASK);
+inline ring_buffer_size_t ring_buffer_num_items(ring_buffer_t *buffer)
+{
+    return ((buffer->head_index - buffer->tail_index) & (buffer->buffer_cap - 1));
 }
 
-#ifdef __cplusplus
+/**
+ * @brief 将共享内存结构体指针设置为NULL。如果该指针是最后一个指向该地址的，
+ * 将导致这块地址无法被再次使用或释放（造成内存泄漏），所以应确保对分配的内存进行妥善管理。
+ * 这个函数仅用于与ring_buffer_assign配对使用。如果使用了ring_buffer_new
+ * 进行对象初始化，则需要使用ring_buffer_destroy对对象进行销毁。
+ * @param buffer - 需要解除引用的指针。请注意这是二级指针，需要传递结构体指针的地址。
+ */
+inline void ring_buffer_detach(ring_buffer_t **buffer)
+{
+    *buffer = NULL;
 }
-#endif
 
 #endif /* RINGBUFFER_H */
